1a8f40527516cc86c1332e9702ab7f0a1cef2bca
Mark Baltes <baltesm@coyote.csusb.edu>
2016-04-29 11:20:54 -0700

    Euler 1 complete.

--- Multiple_3_5.java
+++ Multiple_3_5.java
@@ -1,17 +1,14 @@
 // Project Euler problem: 1
 
-import java.util.*;
-
 public class Multiple_3_5 {
-
-
-
-
-    // public static void main(String[] args) {
-    //     Sieve_of_Eratosthenes s = new Sieve_of_Eratosthenes();
-    //     ArrayList<Integer> p = s.primes(50);
-    //     for (int x : p) {
-    //         System.out.print(x + " ");
-    //     }
-    // }
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.print(sum);
+    }
 }
