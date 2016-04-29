34c78efde162e010b13ffbabcd9ea0e8ba8bdf43
Mark Baltes <baltesm@coyote.csusb.edu>
2016-04-29 10:18:40 -0700

    Changed sieve to use array in algorithm.

--- Multiple_3_5.java
+++ Multiple_3_5.java
@@ -3,13 +3,13 @@
 import java.util.*;
 
 public class Multiple_3_5 {
-    
 
 
-    
+
+
     // public static void main(String[] args) {
     //     Sieve_of_Eratosthenes s = new Sieve_of_Eratosthenes();
-    //     ArrayList<Integer> p = s.primes(25);
+    //     ArrayList<Integer> p = s.primes(50);
     //     for (int x : p) {
     //         System.out.print(x + " ");
     //     }
--- Sieve_of_Eratosthenes.java
+++ Sieve_of_Eratosthenes.java
@@ -5,32 +5,24 @@ import java.util.*;
 
 public class Sieve_of_Eratosthenes {
     public ArrayList<Integer> primes(int n) {
-        ArrayList<Integer> a = new ArrayList<Integer>();
+        int[] a = new int[n + 1];
         ArrayList<Integer> primes = new ArrayList<Integer>();
-        for (int i = 0; i <= n; i++) {
-            a.add(1);
+        for (int i = 0; i < a.length; i++) {
+            a[i] = 1;
         }
 
-        a.set(0, 0);
-        a.set(1, 0);
+        a[0] = 0;
+        a[1] = 0;
 
-        for (int i = 0; i <= n; i++) {
-            if (a.get(i) == 1) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == 1) {
                 primes.add(i);
 
-                for (int j = 0; j <= n; j += i) {
-                    a.set(j, 0);
+                for (int j = i * i; j < a.length; j += i) {
+                    a[j] = 0;
                 }
             }
         }
         return primes;
     }
-
-    // public static void main(String[] args) {
-    //     Sieve_of_Eratosthenes s = new Sieve_of_Eratosthenes();
-    //     ArrayList<Integer> p = s.primes(25);
-    //     for (int x : p) {
-    //         System.out.print(x + " ");
-    //     }
-    // }
 }
