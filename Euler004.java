eb02ab129056fffa9182532e89039073b2a6dc2d
Mark Baltes <baltesm@coyote.csusb.edu>
2018-07-02 16:36:49 -0700

    Cleaned up 001-004.

--- Euler004.java
+++ Euler004.java
@@ -1,48 +1,25 @@
-// Project Euler Problem: 004
-
-// A palindromic number reads the same both ways. The largest palindrome made 
-// from the product of two 2-digit numbers is 9009 = 91 × 99.
-
-// Find the largest palindrome made from the product of two 3-digit numbers.
-
-// Completed 2016/04/29
+/* 
+ * Project Euler Problem: 004
+ * Link: Link: https://projecteuler.net/problem=4
+ *
+ * A palindromic number reads the same both ways. The largest palindrome made 
+ * from the product of two 2-digit numbers is 9009 = 91 * 99.
+ *
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ *
+ * Completed 2016/04/29 
+ */
 
 import java.util.*;
 
 public class Euler004 {
-    // Off top of head.
-    // public boolean isPalindrome(String s) {
-    //     Stack<Character> st = new Stack<Character>();
-    //     for (int j = 0; j < s.length() / 2; j++) {
-    //         st.push(s.charAt(j));
-    //     }
-    //     if (s.length() % 2 == 0) {
-    //         for (int j = s.length() / 2; j < s.length(); j++) {
-    //             Character c;
-    //             c = st.pop();
-    //             if (c != s.charAt(j)) {
-    //                 return false;
-    //             }
-    //         }
-    //         return true;
-    //     } else {
-    //         for (int j = (s.length() / 2) + 1; j < s.length(); j++) {
-    //             Character c;
-    //             c = st.pop();
-    //             if (c != s.charAt(j)) {
-    //                 return false;
-    //             }
-    //         }
-    //         return true;
-    //     }
-    // }
 
-    // Nice one liner.
-    // public boolean isPalindrome(String s) {
-    //     return s.equals(new StringBuilder(s).reverse().toString());
-    // }
+    // One line palindrome check.
+    public boolean isPalindromeOneLiner(String s) {
+        return s.equals(new StringBuilder(s).reverse().toString());
+    }
 
-    // Faster than one liner, and no stack or extra objects. I like this.
+    // Palindrome using custom algorithm. Faster than one liner.
     public static boolean isPalindrome(String s) {
         int n = s.length();
         for (int i = 0; i < n/2; i++) {
@@ -54,12 +31,10 @@ public class Euler004 {
     }
 
     public static void main(String[] args) {
-        // Timing
-        long start = System.nanoTime();
-
-
-        //Euler004 e = new Euler004();
+        
+        long start = System.nanoTime(); // Start timer.
         int largest = 0;
+
         for (int i = 999; i > 100; i--) {
             for (int j = 999; j > 100; j--) {
                 Integer in = new Integer(i * j);
@@ -72,10 +47,10 @@ public class Euler004 {
                 }
             }
         }
+        
         System.out.println(largest);
 
-        // Timing
-        long end = System.nanoTime();
+        long end = System.nanoTime(); // End timer.
         double time = (double)(end - start) / 1000000000;
         System.out.println("Time: " + time + " sec.");
     }
